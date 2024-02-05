af3d88643eda25fad5ceede3193ec8de98a2b53f
Raian Fernandes dos Santos <raianfs@gmail.com>
2024-02-05 08:59:49 -0300

    BKP 02052024

--- bkp_files.sh
+++ bkp_files.sh
@@ -11,9 +11,9 @@ cp ~/.vim/coc-settings.json .
 cp ~/.config/helix/config.toml .
 cp ~/.config/helix/languages.toml .
 
-git checkout main
 DATE=$(date +%m%d%Y)
-echo $DATE
+git checkout $DATE
 git add .
 git commit -m "BKP $DATE"
 git push
+git checkou main
